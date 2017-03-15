98c02b9c8e70028ad3a8ea3cde8e923c98e42652
minhonoh <fortunerain@naver.com>
2017-03-15 19:25:19 +0900

    test

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-console.log(process.env.MICROSOFT_APP_ID);
-console.log(process.env.MICROSOFT_APP_PASSWORD);
-
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
@@ -29,6 +26,31 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+// bot.dialog('/', function (session) {
+//     session.send("Hello World");
+// });
+
+// Make sure you add code to validate these fields
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+const LuisModelUrl = "https://"+luisAPIHostName+"/luis/v2.0/apps/"+luisAppId+"?subscription-key="+luisAPIKey+"&verbose=true&q=";
+// Main dialog with LUIS
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+/*
+.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+*/
+.onDefault((session) => {
+    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+})
+.matches('hello',(session) => {
+    session.send('hi!!');
+})
+.matches('goodbye',(session) => {
+    session.send('goodbye!!');
 });
+//testsdfa
+
+bot.dialog('/', intents);  
